e8f4d32b027796330cf3ae72f6e4b07a8cf7e8ca
aryan <aryansatija2003@gmail.com>
2025-01-01 14:56:08 +0530

    feat: lc_rating_user_data_updated

--- utils/dataset_builder/main.py
+++ utils/dataset_builder/main.py
@@ -45,7 +45,7 @@ def update_excel(data, filename='user_data.xlsx'):
     headers = [
         "Username",
     ]
-    for i in range(1, 51, 1):
+    for i in range(1, 7, 1):
         headers.append(f"contest-{i}")
         
     sheet.append(headers)
@@ -53,26 +53,28 @@ def update_excel(data, filename='user_data.xlsx'):
     for user, user_data in data.items():
         contests = user_data['data']['userContestRankingHistory']
         user_contests = []
-        for contest in contests:
-            if len(user_contests) == 50:
-                break
-            elif contest['attended'] == True:
-                user_contests.append(contest['rating'])
-    
-        row = [user] + user_contests
-        sheet.append(row)
-    
+        try:    
+            threshold = 6        
+            for contest in contests:
+                if contest['attended'] == True:
+                    user_contests.append(contest['rating'])
+                if len(user_contests) >= threshold:
+                    row = [user] + user_contests[-6: ]
+                    threshold += 1 
+                    sheet.append(row)
+        except:
+            print(user, user_data)
     workbook.save(filename)
 
 # Main function to fetch data for random users and update Excel
 def main():
-    usernames = ["__ARYAN1__", "chitraksh24", "xil899", "MenheraCapoo", "harshit_99", "adityaGarg83", "sichern", "fake_name", "PravinGlitch02", "manii15", "Wolfester", "jerksundertaker", "ayushkumarsinha2000", "Kuldeep_Meena", "mrf92", "ruts", "nguyenducloc", "neal_wu", "milind0110", "rajatgoyal376", "lagaHuaHuBro", "01110001", "rajatjc", "NDR0216", "de_coding_life", "redhair_rs", "thunder_strike", "amitbansal13", "LC_Yun"]  
+    usernames = ["__ARYAN1__", "chitraksh24", "dianazhai", "xil899", "MenheraCapoo", "gongyue", "harshit_99", "adityaGarg83", "sichern", "fake_name", "yutingpang", "flyingosprey", "avinash_barnwal", "PravinGlitch02", "manii15", "Wolfester", "jerksundertaker", "sankarkalla4", "yhw99go", "vijayjain", "ankitjainb27", "paru0100", "ayushkumarsinha2000", "jishenli1227", "Kuldeep_Meena", "mrf92", "ruts", "zhengfu23", "nguyenducloc", "neal_wu", "milind0110", "rajatgoyal376", "blisses", "lagaHuaHuBro", "01110001", "rajatjc", "NDR0216", "BearForever", "de_coding_life", "redhair_rs", "thunder_strike", "amitbansal13", "LC_Yun"]  
     user_data = {}
 
     for username in usernames:
         print(f"Fetching data for {username}...")
         user_data[username] = fetch_user_data(username)
-    
+    # print(user_data)
     update_excel(user_data)
 
 if __name__ == "__main__":
