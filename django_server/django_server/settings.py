3a25f3f58512522b5e26ae045fa8211364b5942d
aryan <aryansatija2003@gmail.com>
2025-01-18 17:12:53 +0530

    fix: test-fix

--- django_server/django_server/settings.py
+++ django_server/django_server/settings.py
@@ -54,7 +54,18 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://leetassist.vercel.app"
 ]
-CORS_ALLOW_ALL_ORIGINS = True  
+
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'authorization',
+    'x-requested-with',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://leetassist.vercel.app",
+    "https://leetassist-1.onrender.com",
+]
+
 CORS_ALLOWED_METHODS = [
     'GET',
     'POST',
