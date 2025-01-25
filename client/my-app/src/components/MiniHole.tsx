fe58be9b847d9b7adb74990812b4091aee991835
aryan <aryansatija2003@gmail.com>
2025-01-25 13:46:45 +0530

    fix: mini-loader fixed

--- client/my-app/src/components/MiniHole.tsx
+++ client/my-app/src/components/MiniHole.tsx
@@ -1,7 +1,7 @@
 import "../hole.css";
 const MiniHole = ()=>{
     return ( 
-        <div className="hole w-full h-[150px]">
+        <div className="hole w-full h-[150px] relative">
             <div className="i"></div>
             <div className="i"></div>
             <div className="i"></div>
